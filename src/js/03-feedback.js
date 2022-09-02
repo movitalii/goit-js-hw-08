9eb16a556783b7e1f24d2fc3560d5a1643d75a62
Vitalii Mordovets <vitalii.mordovets@gmail.com>
2022-09-02 23:55:29 +0300

    Task-2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,52 @@
+import throttle from 'lodash.throttle';
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('.feedback-form textarea'),
+}
+
+const STORAGE_KEY = "feedback-form-state";
+
+const formData = {};
+
+refs.form.addEventListener("submit", onFormSubmit);
+refs.textarea.addEventListener("input", throttle(onTextareaInput, 500));
+
+refs.form.addEventListener("input", event => {
+    formData[event.target.name] = event.target.value;
+    console.log(formData);
+})
+
+fillTextarea();
+ 
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+function onTextareaInput(event) {
+    const message = event.target.value;
+    
+    localStorage.setItem(STORAGE_KEY, message)
+}
+
+function fillTextarea() {
+    const savedMassage = localStorage.getItem(STORAGE_KEY);
+
+    if (savedMassage) {
+        refs.textarea.value = savedMassage;
+    }
+}
+
+
+
+
+
+
+
+
+
+
 
