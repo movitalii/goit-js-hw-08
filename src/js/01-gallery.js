eef3093b33a3d0778700675a48ecdb2c1956535e
Vitalii Mordovets <vitalii.mordovets@gmail.com>
2022-09-02 17:49:27 +0300

    Task-1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+
+function createGalleryItemsMarkup(items) {
+    return items.map(({ preview, original, description }) => {
+        return `
+            <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+            </div>
+        `;
+    })
+        .join('');
+}
+
+new SimpleLightbox('.gallery a', {captionsData: "alt",  captionDelay: 250});
+
+
+
