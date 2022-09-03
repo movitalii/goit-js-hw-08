6ed908cf886b0bbf28517dc0acad2b9bcbec97a5
Vitalii Mordovets <vitalii.mordovets@gmail.com>
2022-09-03 16:09:47 +0300

    Task-2, Task-3 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
-import Player from "@vimeo/player";
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(event) {
+    const currentTime = event.seconds;
+    localStorage.setItem("videoplayer-current-time", currentTime);
+
+    if (event.percent === 1) {
+        localStorage.removeItem("videoplayer-current-time");
+    }
+};
+
+setStorageTime();
+
+function setStorageTime() {
+    const savedTime = localStorage.getItem("videoplayer-current-time");
+
+    if (savedTime) {
+        player.setCurrentTime(savedTime);
+    }
+}
+ 
