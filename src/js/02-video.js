30cb927b8babcd1ed16a1877db0f01c9e34bd09d
Vitalii Mordovets <vitalii.mordovets@gmail.com>
2022-09-03 16:12:39 +0300

    HW-8 to check

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,21 +4,23 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
+const STORAGE_KEY = "videoplayer-current-time";
+
 player.on('timeupdate', throttle(onPlay, 1000));
 
 function onPlay(event) {
     const currentTime = event.seconds;
-    localStorage.setItem("videoplayer-current-time", currentTime);
+    localStorage.setItem(STORAGE_KEY, currentTime);
 
     if (event.percent === 1) {
-        localStorage.removeItem("videoplayer-current-time");
+        localStorage.removeItem(STORAGE_KEY);
     }
 };
 
 setStorageTime();
 
 function setStorageTime() {
-    const savedTime = localStorage.getItem("videoplayer-current-time");
+    const savedTime = localStorage.getItem(STORAGE_KEY);
 
     if (savedTime) {
         player.setCurrentTime(savedTime);
